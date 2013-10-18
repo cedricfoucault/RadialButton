e9e9be6c5f9ec3e320ae5c9728b973a10f537984
Cédric Foucault <cedric.foucault@gmail.com>
2013-10-18 18:55:35 +0200

    Initial Commit

---
+++ Radial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Radial
+//
+//  Created by Cédric Foucault on 10/18/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RadialTests/RadialTests.m
@@ -0,0 +1,34 @@
+//
+//  RadialTests.m
+//  RadialTests
+//
+//  Created by Cédric Foucault on 10/18/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RadialTests : XCTestCase
+
+@end
+
+@implementation RadialTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
